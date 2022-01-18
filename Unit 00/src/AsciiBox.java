764898f4199895fa0730217ca269a79af811bb49
AeonianBlitz <56415610+AeonianBlitz@users.noreply.github.com>
2022-01-18 07:31:47 -0800

    Unit 0 Labs Completed

---
+++ Unit 00/src/AsciiBox.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Daniel Choi
+//Date - 1/17/22
+//Class - APCSA
+//Lab  - Ascii Box
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Daniel \t  1/17/22 \n\n" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+	}
+}
---
+++ Unit 00/src/Variables.java
@@ -0,0 +1,62 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Daniel Choi
+//Date - 1/17/22
+//Class - APCSA
+//Lab  - Variables Java
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short short1 = 32000;
+		int int1 = 980000;
+		long long1 = 9999999;
+
+		//decimal variables
+		double double1 = 5.98;
+		float float1 = 9085.28f;
+
+		//other integer types
+		char char1 = 'a';
+
+		//other types
+		boolean boolean1 = true;
+		String string1 = "hello";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne   = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - short1   = "+short1+"\t\t*");
+		System.out.println("*32 bit - int1     = "+int1+"\t\t*");
+		System.out.println("*64 bit - long1    = "+long1+"\t\t*");
+		System.out.println("*32 bit - float1   = "+float1+"\t\t*");
+		System.out.println("*64 bit - double1  = "+double1+"\t\t*");
+		System.out.println("*16 bit - char1    = "+char1+"\t\t\t*");
+		System.out.println("*16 bit - boolean1 = "+boolean1+"\t\t*");
+		System.out.println("*16 bit - string1  = "+string1+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
