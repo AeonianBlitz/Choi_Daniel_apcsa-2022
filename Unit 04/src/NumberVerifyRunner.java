ffe18694880e931dbf1ab757f1e9e270454a0302
AeonianBlitz <56415610+AeonianBlitz@users.noreply.github.com>
2022-01-25 20:52:12 -0800

    Small fix in NumberVerifyRunner

--- Unit 04/src/NumberVerifyRunner.java
+++ Unit 04/src/NumberVerifyRunner.java
@@ -21,7 +21,7 @@ public class NumberVerifyRunner
 		System.out.println("6 is odd :: " + NumberVerify.isOdd(6));
 		System.out.println("6 is even :: " + NumberVerify.isEven(6));
 		System.out.println("1095 is odd :: " + NumberVerify.isOdd(1095));
-		System.out.println("1095is even :: " + NumberVerify.isEven(1095));
+		System.out.println("1095 is even :: " + NumberVerify.isEven(1095));
 		System.out.println("2 is odd :: " + NumberVerify.isOdd(2));
 		System.out.println("2 is even :: " + NumberVerify.isEven(2));
 		System.out.println("99 is odd :: " + NumberVerify.isOdd(99));
